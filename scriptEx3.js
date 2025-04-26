e93e3a168fc049da21a9a45c5db735ea989d9d5f
Celso <celsortc@outlook.com>
2025-04-25 22:26:05 -0300

    começo dos exericcios 3

---
+++ scriptEx3.js
@@ -0,0 +1,28 @@
+// Adicione a classe ativo a todos os itens do menu
+
+const itensMenu = document.querySelectorAll(".menu ul li");
+console.log(itensMenu);
+
+itensMenu.forEach((item) => item.classList.add("ativo"));
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+
+itensMenu.forEach((item) => item.classList.remove("ativo"));
+
+// Verifique se as imagens possuem o atributo alt
+
+const imagens = document.querySelectorAll("img");
+
+imagens.forEach((item, index) => {
+  if (item.hasAttribute("alt")) {
+    console.log(index, "tem alt");
+  } else console.log(index, "n tem alt");
+});
+
+// Modifique o href do link externo no menu
+
+const linkExterno = document.querySelector('.menu ul li [href^="https"]');
+
+linkExterno.href = "https://google.com";
+
+console.log(linkExterno.attributes.href);
