5ce7cb59f40ebecd3aa7cf86e8a1a78f1ea3ec57
Celso <celsortc@outlook.com>
2025-04-28 22:36:33 -0300

    finalizando exercicios e simplificando o primeiro

--- scriptEx5.js
+++ scriptEx5.js
@@ -3,7 +3,7 @@
 // demais itens caso eles possuam a mesma. Previna
 // o comportamento padrão desses links
 
-const links = document.querySelectorAll("a");
+const links = document.querySelectorAll("a[href^='#']");
 
 function verificaClasse(event) {
   event.preventDefault();
@@ -11,13 +11,10 @@ function verificaClasse(event) {
     return;
   } else {
     links.forEach((link) => {
-      if (link.classList.contains("Ativo")) {
-        link.classList.remove("Ativo");
-      }
+      link.classList.remove("Ativo");
     });
-    event.currentTarget.classList.add("Ativo");
   }
-
+  event.currentTarget.classList.add("Ativo");
   console.log(event.target);
 }
 
@@ -28,7 +25,35 @@ links.forEach((item) => {
 // Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
 
+const todosElementos = document.querySelectorAll("body *");
+
+function elemento(event) {
+  console.log(event.target);
+}
+
+todosElementos.forEach((item) => {
+  item.addEventListener("click", elemento);
+});
+
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
 
+function removeElemento(event) {
+  // event.target.remove();
+}
+
+todosElementos.forEach((item) => {
+  item.addEventListener("click", removeElemento);
+});
+
 // Se o usuário clicar na tecla (t), aumente todo o texto do site.
+
+function handleClickT(event) {
+  console.log(event.key);
+
+  if (event.key === "t") {
+    document.documentElement.classList.toggle("textomaior");
+  }
+}
+
+window.addEventListener("keydown", handleClickT);
